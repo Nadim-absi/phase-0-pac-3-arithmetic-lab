d9ca1cba68b8e76633b26de2eb619a81d8f70d23
Nadim Absi <absinad1@gmail.com>
2022-10-07 23:14:53 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function makeInt(n){
+ const parsedInt = parseInt(n,10);
+ return parsedInt
+}
+function preserveDecimal(n){
+    const parsedFloat = parseFloat(n)
+return parsedFloat
+}
+function add(a,b){
+    const result = a+b
+    return result
+}
+function subtract(a,b){
+    const result =a-b
+    return result
+}
+function multiply(a,b){
+    const result =a*b
+    return result
+}
+function divide(a,b){
+    const result = a/b
+    return result
+}
+function increment(n){
+    const result = n+1
+    return result
+}
+function decrement(n){
+    const result =n-1
+    return result
+}
